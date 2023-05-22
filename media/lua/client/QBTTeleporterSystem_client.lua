3ddfd2348ee2546040b189210ac8f95dcec4eaf1
Quest Yarbrough <1403153+Quezion@users.noreply.github.com>
2023-05-22 14:53:22 -0700

    Clientside doesn't crash but can't load offscreen teleporters
    Need to implement custom receiveServerCommand to make client work
    As it doesn't seem possible to load any modData without the isoObject
    and clients don't receive the isoObject unless in physical range

--- media/lua/client/QBTTeleporterSystem_client.lua
+++ media/lua/client/QBTTeleporterSystem_client.lua
@@ -22,7 +22,7 @@ end
 
 --called by java receiveNewLuaObjectAt
 function TpSystem:newLuaObjectAt(x, y, z)
-   self:noise("adding luaObject "..x..','..y..','..z)
+   print("TpSystem:newLuaObjectAt adding luaObject "..x..','..y..','..z)
    local globalObject = self.system:newObject(x, y, z)
    local luaObject = self:newLuaObject(globalObject)
    -- local luaObjectId = luaObject.id
@@ -39,25 +39,25 @@ end
 
 --Client variation of the server "updateTeleporters", might run before new data is received
 function TpSystem.updateTeleportersForClient()
-   self:noise("TpSystem.updateTeleportersForClient(), luaObjectCount == "..TpSystem.instance:getLuaObjectCount())
-   local numTeleporters = TpSystem.instance:getObjectCount()
-   self:noise(string.format("updateTeleportersForClient running on %d teleporters", numTeleporters))
-   for i=0,numTeleporters - 1 do
-      local teleporter = TpSystem.instance.system:getObjectByIndex(i):getModData()
-      self:noise(string.format("/telepoints[%d]: Adding %s %s at: %d, %d, %d",
-                               i,teleporter.id,teleporter.name,teleporter.x,teleporter.y,teleporter.z))
-      qbt.Telepoints.Add(teleporter.id,teleporter.name,teleporter.x,teleporter.y,teleporter.z)
-   end
-   -- for i=1,TpSystem.instance:getLuaObjectCount() do
-   --    local tp = TpSystem.instance:getLuaObjectByIndex(i)
-   --    local isotp = tp:getIsoObject()
-   --    if isotp then
-   --       tp:fromModData(isotp:getModData())
-   --       if tp.id then
-   --          qbt.Telepoints.Add(tp.id,tp.name,isotp:getX(),isotp:getY(),isotp:getZ())
-   --       end
-   --    end
+   print("qbt.TpSystem.updateTeleportersForClient() called")
+   local numTeleporters = TpSystem.instance:getLuaObjectCount()
+   print(string.format("qbt.updateTeleportersForClient running on %d teleporters", numTeleporters))
+   -- for i=0,numTeleporters - 1 do
+   --    local teleporter = TpSystem.instance.system:getObjectByIndex(i):getModData()
+   --    qbt.Telepoints.Add(teleporter.id,teleporter.name,teleporter.x,teleporter.y,teleporter.z)
    -- end
+   for i=1,TpSystem.instance:getLuaObjectCount() do
+      local tp = TpSystem.instance:getLuaObjectByIndex(i)
+      local isotp = tp:getIsoObject()
+      if isotp then
+         tp:fromModData(isotp:getModData())
+         if tp.id then
+            print(string.format("qbt.updateTeleportersForClient/telepoints[%d]: Adding %s %s at: %d, %d, %d",
+                                i,tp.id,tp.name,tp.x,tp.y,tp.z))
+            qbt.Telepoints.Add(tp.id,tp.name,isotp:getX(),isotp:getY(),isotp:getZ())
+         end
+      end
+   end
 end
 
 CGlobalObjectSystem.RegisterSystemClass(TpSystem)
